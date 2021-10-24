fa87040dfeffa0785959ed8dcffaddd99a6b5875
MImranIqbal <se.imraniqbal@gmail.com>
2021-10-24 06:00:38 -0700

    src

--- src/components/Upcoming.jsx
+++ src/components/Upcoming.jsx
@@ -1,4 +1,4 @@
-import { Heading } from "@chakra-ui/layout";
+import { Box, Container, Flex, Heading, Link, Spacer } from "@chakra-ui/layout";
 import Upcomingone from "../assets/upcomming-one.png";
 import Upcomingtwo from "../assets/upcomming-two.jpg";
 import Upcomingthree from "../assets/upcomming-three.jpg";
@@ -6,7 +6,21 @@ import Upcomingfour from "../assets/upcomming-four.jpg";
 import React from "react";
 
 const Upcoming = () => {
-  return <Heading>This is upcoming component</Heading>;
+  return (
+    <Box>
+      <Container maxW="1100" my="20">
+        <Flex border="1px solid grey">
+          <Heading as="h3" alignItems="center" size="lg">
+            Upcoming online events
+          </Heading>
+          <Spacer />
+          <Link fontWeight="semibold" fontSize="20" color="teal.500">
+            Explore more events
+          </Link>
+        </Flex>
+      </Container>
+    </Box>
+  );
 };
 
 export default Upcoming;
