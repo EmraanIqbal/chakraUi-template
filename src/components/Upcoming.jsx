155f6892c3805c35d2e3eff838f9da65dc77eb91
MImranIqbal <se.imraniqbal@gmail.com>
2021-10-24 04:43:07 -0700

    src

--- src/components/Upcoming.jsx
+++ src/components/Upcoming.jsx
@@ -1,4 +1,8 @@
 import { Heading } from "@chakra-ui/layout";
+import Upcomingone from "../assets/upcomming-one.png";
+import Upcomingtwo from "../assets/upcomming-two.jpg";
+import Upcomingthree from "../assets/upcomming-three.jpg";
+import Upcomingfour from "../assets/upcomming-four.jpg";
 import React from "react";
 
 const Upcoming = () => {
