88386f0f7fa53dd2d9bfcd74056689a7dde76cc7
MImranIqbal <se.imraniqbal@gmail.com>
2021-10-28 11:59:39 -0700

    src

--- src/components/Upcoming.jsx
+++ src/components/Upcoming.jsx
@@ -235,7 +235,7 @@ const Upcoming = () => {
                 <path d="M11.5 4.5A1.5 1.5 0 0113 6v1.4L16 5v8l-3-2.4V12a1.5 1.5 0 01-1.5 1.5h-8A1.5 1.5 0 012 12V6a1.5 1.5 0 011.5-1.5h8z"></path>
               </svg>
               <Box ml="2" color="#757575" fontWeight="semibold">
-                Online Even
+                Online Event
               </Box>
             </Box>
             <Box color="#aaa79d" m="10px 10px 2px 10px">
